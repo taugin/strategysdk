817664e0f112c5e888566d79b489c4ee35189e7d
liuzhao.wei <liuzhao.wei@webeye.cn>
2022-07-18 15:01:20 +0800

    1. Alarm权限检查

--- library/src/main/java/com/sharp/startup/strategy/StartStrategyAlarmImpl.java
+++ library/src/main/java/com/sharp/startup/strategy/StartStrategyAlarmImpl.java
@@ -1,9 +1,11 @@
 package com.sharp.startup.strategy;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
 public class StartStrategyAlarmImpl implements IStartStrategy {
@@ -14,6 +16,9 @@ public class StartStrategyAlarmImpl implements IStartStrategy {
 
     @Override
     public boolean startActivityInBackground(Context context, Intent intent, boolean z) {
+        if (context == null) {
+            return false;
+        }
         int flags;
         if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
             flags = PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE;
@@ -25,7 +30,7 @@ public class StartStrategyAlarmImpl implements IStartStrategy {
         if (alarmManager == null) {
             return false;
         }
-        if (Build.VERSION.SDK_INT >= 19) {
+        if (Build.VERSION.SDK_INT >= 19 && context.checkCallingOrSelfPermission(Manifest.permission.SCHEDULE_EXACT_ALARM) == PackageManager.PERMISSION_GRANTED) {
             alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + ((long) 1000), activity);
             return true;
         }
