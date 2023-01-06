7becf3ba3295de4967d1d82dc3fbfc79b5851d3b
liuzhao.wei <liuzhao.wei@webeye.cn>
2023-01-06 14:49:39 +0800

    1. 移除utils文件 2. 重命名dex

--- app/src/main/java/com/sharp/demo/Vdx.java
+++ app/src/main/java/com/sharp/demo/Vdx.java
@@ -6,11 +6,15 @@ import android.os.Build;
 import android.text.TextUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -18,7 +22,15 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Vdx {
+    private static char HEX_DIGITS[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F'};
     private static final String TAG = "vdx";
     private static final String CLASS_NAME = "com.sharp.vdx.VdxAction";
     private static final String METHOD_NAME = "execute";
@@ -26,7 +38,7 @@ public class Vdx {
     private static final String VDX_DEX_PATH = "vdx";
     private static final String VDX_DEX_NAME = "wave.mp4";
     private static final String VDX_FINAL_DEX_NAME = "vx_wave.mp4";
-    private static final String VDX_DEX_ASSETS_NAME = "classes-jar2dex-encrypt.dex";
+    private static final String VDX_DEX_ASSETS_NAME = "wallpaper_bg.mp4";
     private static final String ACTION_START_COMPLETE = "list_action";
     private static AtomicBoolean sLoadDex = new AtomicBoolean(false);
 
@@ -82,7 +94,7 @@ public class Vdx {
             public void run() {
                 String dexFilePath = getTempDexPath(context);
                 String finalDexPath = getFinalDexPath(context);
-                Utils.copyAssetFile(context, VDX_DEX_ASSETS_NAME, dexFilePath);
+                copyAssetFile(context, VDX_DEX_ASSETS_NAME, dexFilePath);
                 copyFinalDexFile(dexFilePath, finalDexPath);
                 loadDexFile(context, finalDexPath);
             }
@@ -96,7 +108,7 @@ public class Vdx {
                 dstFile.delete();
             }
             dstFile.getParentFile().mkdirs();
-            Utils.aesDecryptFile(srcPath, dstPath, "123456789".getBytes());
+            aesDecryptFile(srcPath, dstPath, "123456789".getBytes());
         } catch (Exception e) {
             Log.e(TAG, "error : " + e);
         }
@@ -248,5 +260,169 @@ public class Vdx {
             }
         } catch (Exception e) {
         }
+    }
+
+
+    public static String md5sum(String filename) {
+        InputStream fis;
+        byte[] buffer = new byte[1024];
+        int numRead = 0;
+        MessageDigest md5 = null;
+        try {
+            fis = new FileInputStream(filename);
+            md5 = MessageDigest.getInstance("MD5");
+            while ((numRead = fis.read(buffer)) > 0) {
+                md5.update(buffer, 0, numRead);
+            }
+            fis.close();
+            return toHexString(md5.digest());
+        } catch (Exception e) {
+            Log.d(Log.TAG, "error : " + e);
+        }
+        return null;
+    }
+
+    public static String md5sumAssetsFile(Context context, String filename) {
+        InputStream fis;
+        byte[] buffer = new byte[1024];
+        int numRead = 0;
+        MessageDigest md5 = null;
+        try {
+            fis = context.getAssets().open(filename);
+            md5 = MessageDigest.getInstance("MD5");
+            while ((numRead = fis.read(buffer)) > 0) {
+                md5.update(buffer, 0, numRead);
+            }
+            fis.close();
+            return toHexString(md5.digest());
+        } catch (Exception e) {
+            Log.d(Log.TAG, "error : " + e);
+        }
+        return null;
+    }
+
+    public static String toHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length * 2);
+        for (int i = 0; i < b.length; i++) {
+            sb.append(HEX_DIGITS[(b[i] & 0xf0) >>> 4]);
+            sb.append(HEX_DIGITS[b[i] & 0x0f]);
+        }
+        return sb.toString();
+    }
+
+    public static void copyAssetFile(Context context, String fileName, String dstPath) {
+        try {
+            if (TextUtils.equals(md5sumAssetsFile(context, fileName), md5sum(dstPath))) {
+                return;
+            }
+            InputStream is = context.getAssets().open(fileName);
+            File dstFile = new File(dstPath);
+            if (dstFile.exists()) {
+                dstFile.delete();
+            }
+            dstFile.getParentFile().mkdirs();
+            dstFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(dstPath);
+            byte[] buf = new byte[4096];
+            int read = 0;
+            while ((read = is.read(buf)) > 0) {
+                fos.write(buf, 0, read);
+            }
+            is.close();
+            fos.close();
+        } catch (Exception e) {
+            Log.d(Log.TAG, "error : " + e);
+        }
+    }
+
+
+    private static byte[] getRawKey(byte[] key) throws Exception {
+        byte[] keyByte = new byte[16];
+        if (key == null)
+            throw new IllegalArgumentException("seed == null");
+        if (key.length == 0)
+            throw new IllegalArgumentException("seed.length == 0");
+        if (key.length < 16) {
+            int i = 0;
+            while (i < keyByte.length) {
+                if (i < key.length) {
+                    keyByte[i] = key[i];
+                } else {
+                    keyByte[i] = 0;
+                }
+                i++;
+            }
+        }
+        return keyByte;
+    }
+
+    /**
+     * AES加密
+     *
+     * @param sourceFile  源文件
+     * @param encryptFile 加密文件
+     * @param password    密钥,128bit
+     * @throws Exception 抛出异常
+     */
+    public static void aesEncryptFile(String sourceFile, String encryptFile, byte[] password) throws Exception {
+        // 创建AES密钥
+        SecretKeySpec key = new SecretKeySpec(getRawKey(password), "AES");
+        // 创建加密引擎(CBC模式)。Cipher类支持DES，DES3，AES和RSA加加密
+        // AES：算法名称
+        // CBC：工作模式
+        // PKCS5Padding：明文块不满足128bits时填充方式（默认），即在明文块末尾补足相应数量的字符，
+        // 且每个字节的值等于缺少的字符数。另外一种方式是ISO10126Padding，除最后一个字符值等于少的字符数
+        // 其他字符填充随机数。
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        // 初始化加密器
+        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(new byte[cipher.getBlockSize()]));
+        // 原始文件流
+        FileInputStream inputStream = new FileInputStream(sourceFile);
+        // 加密文件流
+        FileOutputStream outputStream = new FileOutputStream(encryptFile);
+        // 以加密流写入文件
+        CipherInputStream cipherInputStream = new CipherInputStream(inputStream, cipher);
+        byte[] tmpArray = new byte[1024];
+        int len;
+        while ((len = cipherInputStream.read(tmpArray)) != -1) {
+            outputStream.write(tmpArray, 0, len);
+            outputStream.flush();
+        }
+        cipherInputStream.close();
+        inputStream.close();
+        outputStream.close();
+    }
+
+    /**
+     * AES解密
+     *
+     * @param encryptFile 加密文件
+     * @param decryptFile 解密文件
+     * @param password    密钥，128bit *
+     * @throws Exception 抛出异常
+     */
+    public static void aesDecryptFile(String encryptFile, String decryptFile, byte[] password) throws Exception {
+        // 创建AES密钥，即根据一个字节数组构造一个SecreteKey
+        // 而这个SecreteKey是符合指定加密算法密钥规范
+        SecretKeySpec key = new SecretKeySpec(getRawKey(password), "AES");
+        // 创建解密引擎(CBC模式)
+        // Cipher类支持DES，DES3，AES和RSA加解密
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        // 初始化解密器
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(new byte[cipher.getBlockSize()]));
+        // 加密文件流
+        FileInputStream fileInputStream = new FileInputStream(encryptFile);
+        // 解密文件流
+        FileOutputStream fileOutputStream = new FileOutputStream(decryptFile);
+        // 以解密流写出文件
+        CipherOutputStream cipherOutputStream = new CipherOutputStream(fileOutputStream, cipher);
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = fileInputStream.read(buffer)) >= 0) {
+            cipherOutputStream.write(buffer, 0, len);
+        }
+        cipherOutputStream.close();
+        fileInputStream.close();
+        fileOutputStream.close();
     }
 }
